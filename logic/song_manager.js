26b91b12e0ac2b6ad10179f1b054ee36afaa0131
PabloCarrascosa <78744481+PabloCarrascosa@users.noreply.github.com>
2022-11-01 13:36:50 +0100

    añadida funcionalidades playlist + perfil

---
+++ logic/song_manager.js
@@ -0,0 +1,46 @@
+const draggables = document.querySelectorAll('.cancion-item')
+const containers = document.querySelectorAll('.listado-canciones')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.cancion-item:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+function removeElement(id){
+  var removable = document.getElementById(id);
+  var listado = removable.parentNode;
+  listado.removeChild(removable);
+
+}
